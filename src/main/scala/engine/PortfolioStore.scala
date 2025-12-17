57c98e9ca6e181d8f5ddfa153e8b7db2190e798e
Leonardo Chiaretti <160722347+cicixgliamici@users.noreply.github.com>
2025-12-17 21:32:57 +0100

    Add cash

--- src/main/scala/engine/PortfolioStore.scala
+++ src/main/scala/engine/PortfolioStore.scala
@@ -5,22 +5,30 @@ import java.nio.file.{Files, Path}
 import java.nio.charset.StandardCharsets.UTF_8
 
 trait PortfolioStore {
-  def load(): Map[String, BigDecimal]
-  def save(pf: Map[String, BigDecimal]): Unit
+  def load(): PortfolioState
+  def save(pf: PortfolioState): Unit
+}
+
+final case class PortfolioState(positions: Map[String, BigDecimal], cash: BigDecimal) {
+  def withDefaults: PortfolioState = copy(positions = positions.withDefaultValue(BigDecimal(0)))
+  def onlyPositivePositions: PortfolioState = copy(positions = positions.filter(_._2 > 0))
 }
 
 final case class FileJsonPortfolioStore(path: Path) extends PortfolioStore {
   import frontend.PortfolioJson._
-  override def load(): Map[String, BigDecimal] =
-    if (!Files.exists(path)) Map.empty.withDefaultValue(BigDecimal(0))
+  override def load(): PortfolioState =
+    if (!Files.exists(path)) PortfolioState(Map.empty.withDefaultValue(BigDecimal(0)), BigDecimal(0))
     else {
       val json = Files.readString(path, UTF_8)
       val pj   = read[PortfolioJ](json)
-      pj.positions.withDefaultValue(BigDecimal(0))
+      PortfolioState(
+        positions = pj.positions.withDefaultValue(BigDecimal(0)),
+        cash = pj.cash.getOrElse(BigDecimal(0))
+      )
     }
 
-  override def save(pf: Map[String, BigDecimal]): Unit = {
-    val json = write(PortfolioJ(pf.filter(_._2 > 0)), indent = 2)
+  override def save(pf: PortfolioState): Unit = {
+    val json = write(PortfolioJ(pf.positions.filter(_._2 > 0), Some(pf.cash)), indent = 2)
     Files.writeString(path, json, UTF_8)
   }
 }
