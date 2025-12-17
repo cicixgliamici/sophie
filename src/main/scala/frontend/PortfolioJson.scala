72d4d2cff7b6420d05cc93815e0ee5f7d95efb05
Leonardo Chiaretti <160722347+cicixgliamici@users.noreply.github.com>
2025-12-17 21:33:53 +0100

    Add cash

--- src/main/scala/frontend/PortfolioJson.scala
+++ src/main/scala/frontend/PortfolioJson.scala
@@ -4,6 +4,9 @@ import upickle.default._
 import frontend.MdJsonCodec.bigDecimalRW // riusa il codec BigDecimal tollerante
 
 object PortfolioJson {
-  final case class PortfolioJ(positions: Map[String, BigDecimal])
+  final case class PortfolioJ(
+      positions: Map[String, BigDecimal],
+      cash: Option[BigDecimal] = None // optional for backward compatibility
+  )
   implicit val pfRw: ReadWriter[PortfolioJ] = macroRW
 }
