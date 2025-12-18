2906655ca7d6d65e281c9d24af0ee37964b84c6c
Leonardo Chiaretti <160722347+cicixgliamici@users.noreply.github.com>
2025-12-18 18:55:56 +0100

    Comments
    Removed redundant comment about the main method.

--- src/main/scala/TuiMain.scala
+++ src/main/scala/TuiMain.scala
@@ -1,6 +1,5 @@
 /**
  * Entry point for the Sophie application in Text User Interface (TUI) mode.
- * This object contains the main method, which starts the TUI by invoking SophieTui.run().
  * It serves as the launcher for the interactive console interface of the software.
  */
 
