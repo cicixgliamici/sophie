96af051cc847d86e7044069871b2677d0d1d49dc
Leonardo Chiaretti <160722347+cicixgliamici@users.noreply.github.com>
2025-12-18 19:46:51 +0100

    Comments

--- src/test/scala/frontend/EndToEndTradingSpec.scala
+++ src/test/scala/frontend/EndToEndTradingSpec.scala
@@ -14,12 +14,11 @@ class EndToEndTradingSpec extends AnyFunSuite {
     val prog = "BUY 100 EUR OF MSFT"
     val res = ProgramEvaluator.evaluate(prog, md)
     val plan = res.plan
-    val pm = new PortfolioManager(sym => md.price(sym), DummyPrinter)
+    val pm = new PortfolioManager()
 
     // apply plan via PortfolioManager
-    val applied = pm.applyPlan(Some(plan), sym => md.price(sym))
-    assert(applied == 1)
-    val pf = pm.getPortfolio
+    val (afterBuy, _) = pm.applyPlan(Some(plan), sym => md.price(sym), pm.empty)
+    val pf = afterBuy.positions
     // 100 EUR / 100 = qty 1
     assert(pf("MSFT") == BigDecimal(1))
 
@@ -27,15 +26,9 @@ class EndToEndTradingSpec extends AnyFunSuite {
     val prog2 = "SELL 50 EUR OF MSFT"
     val res2 = ProgramEvaluator.evaluate(prog2, md)
     val plan2 = res2.plan
-    val applied2 = pm.applyPlan(Some(plan2), sym => md.price(sym))
-    assert(applied2 == 1)
-    val pf2 = pm.getPortfolio
+    val (afterSell, _) = pm.applyPlan(Some(plan2), sym => md.price(sym), afterBuy)
+    val pf2 = afterSell.positions
     // selling 50 EUR at price 100 reduces qty by 0.5 (from 1 -> 0.5)
     assert(pf2("MSFT") == BigDecimal(0.5))
   }
-}
-
-// reuse DummyPrinter from previous test file
-object DummyPrinter extends TuiPrinter {
-  def printlnLine(s: String): Unit = ()
 }
